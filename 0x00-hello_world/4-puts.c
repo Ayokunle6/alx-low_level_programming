21d30c8cf30a368e0916da13ddd72664db9a13e4
Adediji Ayokunle <adedijikunle6689@gmail.com>
2022-06-05 04:18:06 -0700

    my first C programming using PUTS function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Using the PUTS function(entry of program)
+ *
+ *Description: The main prnts out the puts function 
+ * "Programming is like building a multilingual puzzle
+ * Return: 0
+ */
+int main(void)
+{
+  puts("\"Programming is like building a multilingual puzzle");
+  return (0):
+}
