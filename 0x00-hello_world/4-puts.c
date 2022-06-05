497131604d535adbeaf1f050fc72050443ec99ab
Adediji Ayokunle <adedijikunle6689@gmail.com>
2022-06-05 04:58:35 -0700

    C programmming usinr the puts function update

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 /**
  * main - Using the PUTS function(entry of program)
- *Description:The main prnts out the puts function 
+ * Description:The main prnts out the puts function 
  * "Programming is like building a multilingual puzzle
  * Return: 0
  */
 int main(void)
 {
- puts("\"Programming is like building a multilingual puzzle");
- return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
