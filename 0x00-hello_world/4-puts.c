7a86327015b169145f1a483e71805db53a448262
Adediji Ayokunle <adedijikunle6689@gmail.com>
2022-06-05 05:18:28 -0700

    C programming using puts instead of PRINTF

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 /**
  * main - Using the PUTS function(entry of program)
- * Description (The main prnts out the puts function) 
- * "Programming is like building a multilingual puzzle
- * Return: 0
- */
+  * Description: it prints out "Programming is like building a multilingual puzzle
+  * Return: 0
+  */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
