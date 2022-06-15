2ab5c82f593b381b0240143af9b2bf833f7132b8
Adediji Ayokunle <adedijikunle6689@gmail.com>
2022-06-15 07:14:10 -0700

    printing last digit of number stored

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -15,10 +15,16 @@ n = rand() - RAND_MAX / 2;
 /* The code goes here */
 num = n % 9;
 if(num > 5)
+{
 printf("last digit of %d is %d and is greater than 5\n", n, num);
+}
 else if(num == 0)
+{
 printf("last digit of %d is %d and is 0\n", n, num);
+}
 else
+{
 printf("Last digit of %d is %d and is less than 6 and not 0\n", n, num);
+}
 return (0);
 }
