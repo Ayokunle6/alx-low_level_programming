11c7a671d9102722cf531861aae5a942fbd11332
Adediji Ayokunle <adedijikunle6689@gmail.com>
2022-06-23 07:11:01 -0700

    prints all possible different combinations of three digits.

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -8,20 +8,18 @@
 
 int main(void)
 {
-int n1, n2, n3;
+int numb1, numb2, numb3;
 
-for (n1 = '0'; n1 <= '7'; n1++)
-
+for (numb1 = '0'; numb1 <= '7'; numb1++)
 {
-for (n2 = n1 + '1'; n2 <= '8'; n2++)
-
+for (numb2 = numb1 + 1; numb2 <= '8'; numb2++)
 {
-for (n3 = n2 + '1'; n3 <= '9'; n3++)
+for (numb3 = numb2 + 1; numb3 <= '9'; numb3++)
 {
-putchar(n1);
-putchar(n2);
-putchar(n3);
-if (n1 == '7' && n2 == '8' && n3 == '9')
+putchar(numb1);
+putchar(numb2);
+putchar(numb3);
+if (numb1 == '7' && numb2 == '8' && numb3 == '9')
 break;
 putchar(',');
 putchar(' ');
