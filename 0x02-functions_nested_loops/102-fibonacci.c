df91a87031671a3f6595a76437cf1f4c76e258d6
Adediji Ayokunle <adedijikunle6689@gmail.com>
2022-06-25 17:27:59 -0700

    prints the first 50 Fibonacci numbers, starting with 1 and 2

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* main - prints the first 50 Fibonacci numbers, starting with 1 and 2)
+*
+* Return: 0
+*/
+
+int main(void)
+{
+long int fib1, fib2, sum, a;
+fib1 = 1;
+fib2 = 2;
+sum = 3;
+
+printf("%ld, ", fib1);
+printf("%ld, ", fib2);
+for (a = 3; a <= 50; a++)
+{
+printf("%ld, ", sum);
+fib1 = fib2;
+fib2 = sum;
+sum = fib1 + fib2;
+}
+printf("%ld\n", sum);
+return (0);
+}
