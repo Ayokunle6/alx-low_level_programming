d9ecd3fbaa8aa9c0f23b9144641e2be5921421fb
Ayokunle6 <adedijikunle6689@gmail.com>
2022-07-05 01:19:19 +0100

    prints the name of the file it was compiled from

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include "1-pi.h"
+#include <stdio.h>
+
+/**
+* main - prints the name of the file it was compiled from,
+* 
+* Return: 0
+*/
+
+int main(void)
+{
+printf("%s\n", __FILE__);
+return (0);
+}
